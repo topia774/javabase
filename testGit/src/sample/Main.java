87555821476569a6c5b78698b56000d9a86a04b6
topia774 <topia774@outlook.jp>
2020-07-16 12:52:01 +0900

    九九の計算結果を追加

--- testGit/src/sample/Main.java
+++ testGit/src/sample/Main.java
@@ -3,6 +3,8 @@ package sample;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("九九の計算結果");
+		test.test12();
 		System.out.println("Hello GitHub!");
 		System.out.println("コネクト");
 	}
---
+++ testGit/src/sample/test.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class test {
+	public static void test12() {
+        for(int i = 1; i < 10; i++) {
+            for(int j = 1; j < 10; j++) {
+            System.out.print(i + "×" + j + "=" + i * j);
+            System.out.println("");
+
+        }
+            System.out.println("");
+        }
+    }
+
+}
